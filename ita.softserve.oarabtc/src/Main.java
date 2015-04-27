e01a9673dd3c9650eac0f64bab41e4781a9848be
olya-arabchuk <olya.arabchuk@gmail.com>
2015-04-27 23:52:18 +0300

    fixed code

--- ita.softserve.oarabtc/src/Main.java
+++ ita.softserve.oarabtc/src/Main.java
@@ -5,12 +5,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        fixedPayEm A1 = new fixedPayEm (1, "Bilbo", "Beggins", 500);
-        fixedPayEm B1 = new fixedPayEm (2, "Oleg", "Nadraga", 850);
+        FixedPayEmployee A1 = new FixedPayEmployee(1, "Bilbo", "Beggins", 500);
+        FixedPayEmployee B1 = new FixedPayEmployee(2, "Oleg", "Nadraga", 850);
 
-        hourlyPayEm A2 = new hourlyPayEm(99, "Anakin", "Skywalker", 3.1);
-        hourlyPayEm B2 = new hourlyPayEm(98, "QuiGon", "Jinn", 4.1);
-        hourlyPayEm C2 = new hourlyPayEm(97, "Padme", "Amidala", 7.0);
+        HourlyPayEmployee A2 = new HourlyPayEmployee(99, "Anakin", "Skywalker", 3.1);
+        HourlyPayEmployee B2 = new HourlyPayEmployee(98, "QuiGon", "Jinn", 4.1);
+        HourlyPayEmployee C2 = new HourlyPayEmployee(97, "Padme", "Amidala", 7.0);
     }
 
 }
