94e8aaadd3210dffe2d5ae96c288537ef4a862b1
igor <ihor.ilnytskyi@gmail.com>
2015-04-28 00:14:49 +0300

    Training of regular expressions.

---
+++ ita.softserve.iil'ntc/regexp/src/PhoneBookCheckingClass.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneBookCheckingClass {
+    public static void main (String [] args) {
+
+        Pattern reTemplate = Pattern.compile("\\(?\\d+\\)?\\D?\\d+\\D?\\d+");
+        TreeMap<Integer, String> myPhoneBook = new TreeMap<>();
+        myPhoneBook.put(1, "123 123 7777");
+        myPhoneBook.put(2, "(123) 456 7899");
+        myPhoneBook.put(3, "(123).456.7899");
+        myPhoneBook.put(4, "(123)-456-7899");
+        myPhoneBook.put(5, "123-456-7899");
+        myPhoneBook.put(6, "123 456 7899");
+        myPhoneBook.put(8, "1234567899");
+        myPhoneBook.put(9, "fu*k");
+        myPhoneBook.put(10, "HNY");
+        myPhoneBook.put(11, "www.google.com.ua");
+
+        for (Map.Entry<Integer, String> phoneNumber : myPhoneBook.entrySet()) {
+            Matcher phoneExample = reTemplate.matcher(phoneNumber.getValue());
+            System.out.println(phoneNumber.getKey() + " number - " + phoneNumber.getValue() + " - " + phoneExample.matches() + " number");
+        }
+    }
+}
