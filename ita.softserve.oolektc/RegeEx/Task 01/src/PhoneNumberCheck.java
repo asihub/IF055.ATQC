c31ab74977f9f4a16470ef709d09377f857b02f8
ErevosOz <gobland@mail.ru>
2015-04-28 12:25:28 +0300

    Improved regex pattern, added another phone number check

--- ita.softserve.oolektc/RegeEx/Task 01/src/PhoneNumberCheck.java
+++ ita.softserve.oolektc/RegeEx/Task 01/src/PhoneNumberCheck.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class PhoneNumberCheck {
 
     public static boolean isPhoneNumberValid(String phoneNumber){
-        Pattern pattern = Pattern.compile("(\\(?\\d{3}\\)?[\\s\\.\\-]?){2}\\d{4}");
+        Pattern pattern = Pattern.compile("\\(?\\d{3}\\)?((\\s?\\d{3}\\s?)|(\\.?\\d{3}\\.?)|(\\-?\\d{3}\\-?))\\d{4}");
         Matcher matcher = pattern.matcher(phoneNumber);
         return matcher.matches();
     }
@@ -23,6 +23,7 @@ public class PhoneNumberCheck {
             add("123456789910");
             add("911");
             add("067-56-5412");
+            add("(123).456-7899");
             add("http://www.regexr.com");
         }};
 
