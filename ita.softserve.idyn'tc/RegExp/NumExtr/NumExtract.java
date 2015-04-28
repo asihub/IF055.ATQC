4364f81a3d7220aac75d11258e828cfdcce0a453
Ihor Dynka <ihor-dynka@gmail.com>
2015-04-29 00:49:31 +0300

    IF055.ATQC

---
+++ ita.softserve.idyn'tc/RegExp/NumExtr/NumExtract.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class NumExtract {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("\\d*\\.?\\d+");
+        Matcher matcher = pattern.matcher("Hello 2.15 digital World 5,3");
+
+        List<String> numbers=new ArrayList<String>();
+
+        while (matcher.find()){
+            numbers.add(matcher.group());
+        }
+        if (numbers.isEmpty()){
+            System.out.println("There are no numbers in this string");
+        }else {
+            System.out.println("Numbers from this string: " +numbers);
+        }
+
+    }
+}
+
+
+
+
+
