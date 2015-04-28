3455b9a9feb1373ca9765ef43dfb3d4fce1c08bf
ErevosOz <gobland@mail.ru>
2015-04-28 10:00:24 +0300

    Added regular expression task 02

---
+++ ita.softserve.oolektc/RegeEx/Task 02/src/NumberExtractor.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NumberExtractor {
+
+    public static void main(String[] args) {
+
+        Pattern pattern = Pattern.compile("\\d*\\.?\\d+");
+        Matcher matcher = pattern.matcher("Lorem 2.15 ipsum 5,3,45 dolor 5,7 sit amet,3rd consectetur adipiscing 15th");
+
+        List<String> numbers = new ArrayList<>();
+
+        while (matcher.find()){
+            numbers.add(matcher.group());
+        }
+
+        String[] extractedNumbers = new String[numbers.size()];
+        extractedNumbers = numbers.toArray(extractedNumbers);
+
+        String result = Arrays.toString(extractedNumbers);
+        System.out.println(result);
+
+    }
+}
