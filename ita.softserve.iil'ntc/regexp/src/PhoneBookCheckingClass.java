2df896af398b3e92177b830fd2739bda07ea655e
igor <ihor.ilnytskyi@gmail.com>
2015-04-29 10:27:03 +0300

    One class has been changed.

--- ita.softserve.iil'ntc/regexp/src/PhoneBookCheckingClass.java
+++ ita.softserve.iil'ntc/regexp/src/PhoneBookCheckingClass.java
@@ -4,10 +4,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneBookCheckingClass {
-    double i = .4;
+
     public static void main (String [] args) {
 
-        Pattern reTemplate = Pattern.compile("(^(\\(?\\d{3}\\)?)((\\.\\d{3}\\.)|(\\-\\d{3}\\-)|(\\s\\d{3}\\s))\\d{4})|\\d{10}");
         TreeMap<Integer, String> myPhoneBook = new TreeMap<>();
         myPhoneBook.put(1, "123?123?7777");
         myPhoneBook.put(2, "(123) 456.7899");
@@ -21,10 +20,18 @@ public class PhoneBookCheckingClass {
         myPhoneBook.put(10, "HNY");
         myPhoneBook.put(11, "www.google.com.ua");
         myPhoneBook.put(12, "(123)x456x7899");
+        myPhoneBook.put(13, "a12345678991");
 
         for (Map.Entry<Integer, String> phoneNumber : myPhoneBook.entrySet()) {
-            Matcher andPhoneNumberExample = reTemplate.matcher(phoneNumber.getValue());
-            System.out.println(phoneNumber.getKey() + " number - " + phoneNumber.getValue() + " - " + andPhoneNumberExample.matches() + " number.");
+            System.out.println(phoneNumber.getKey() + " number - " + phoneNumber.getValue() + " - " + checkThePhone(phoneNumber.getValue()) + " number.");
         }
+
     }
+
+    public static boolean checkThePhone(String phoneNumberValue) {
+        Pattern regexTemplate = Pattern.compile("(^(\\(?\\d{3}\\)?)((\\.\\d{3}\\.)|(\\-\\d{3}\\-)|(\\s\\d{3}\\s))\\d{4})|\\d{10}");
+        Matcher inputtedPhoneNumberExample = regexTemplate.matcher(phoneNumberValue);
+        return inputtedPhoneNumberExample.matches();
+    }
+
 }
