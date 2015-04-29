385ecc9edc1fc34905592695d19b2aef60adc43d
ErevosOz <gobland@mail.ru>
2015-04-29 14:15:54 +0300

    Refactored regex task 2, added null check

--- ita.softserve.oolektc/RegeEx/Task 02/src/NumberExtractor.java
+++ ita.softserve.oolektc/RegeEx/Task 02/src/NumberExtractor.java
@@ -1,27 +1,34 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NumberExtractor {
 
+    public static String stringWithNumbers = "Lorem 2.15 ipsum 5,3,45 dolor 5,7 sit amet,3rd consectetur adipiscing 15th";
+    public static String stringWithoutNumbers = "Lorem ipsum dolor sit amet,consectetur adipiscing";
+
     public static void main(String[] args) {
 
+        System.out.println(extractNumbers(stringWithNumbers));
+        System.out.println(extractNumbers(stringWithoutNumbers));
+
+    }
+
+    public static List extractNumbers(String string) {
         Pattern pattern = Pattern.compile("\\d*\\.?\\d+");
-        Matcher matcher = pattern.matcher("Lorem 2.15 ipsum 5,3,45 dolor 5,7 sit amet,3rd consectetur adipiscing 15th");
+        Matcher matcher = pattern.matcher(string);
 
-        List<String> numbers = new ArrayList<>();
+        List<String> listOfNumbers = new ArrayList<>();
 
-        while (matcher.find()){
-            numbers.add(matcher.group());
+        while (matcher.find()) {
+            listOfNumbers.add(matcher.group());
         }
 
-        String[] extractedNumbers = new String[numbers.size()];
-        extractedNumbers = numbers.toArray(extractedNumbers);
-
-        String result = Arrays.toString(extractedNumbers);
-        System.out.println(result);
-
+        if (listOfNumbers.isEmpty()){
+            return null;
+        } else {
+            return listOfNumbers;
+        }
     }
 }
