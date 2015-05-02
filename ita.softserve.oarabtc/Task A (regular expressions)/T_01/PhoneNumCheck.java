799bacbe7a6e746b6d26e98e782f1ba6a4c247fe
olya-arabchuk <olya.arabchuk@gmail.com>
2015-05-03 01:17:08 +0300

    RegExp prime version

--- ita.softserve.oarabtc/Task A (regular expressions)/T_01/PhoneNumCheck.java
+++ ita.softserve.oarabtc/Task A (regular expressions)/T_01/PhoneNumCheck.java
@@ -2,21 +2,16 @@
  * Created by Olia on 02.05.2015.
  */
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-import java.util.ArrayList;
+
+import java.util.regex.*;
 
 public class PhoneNumCheck {
 
-    public static boolean phoneNumberCheck (String phoneNumber) {
-        Pattern numPattern = Pattern.compile("(\\(?\\d{3})?\\s?|.?|-?\\d{3}\\s?|.?|-?\\d{3}");
-        Matcher numMatcher = numPattern.matcher(phoneNumber);
-        return numMatcher.matches();
+    public static void main (String[] args) {
+
+        Pattern p = Pattern.compile("(\\(?\\d{3})?[\\s|.|-]?\\d{3}[\\s|.|-]?\\d{3}");
+        Matcher m = p.matcher("(123.456-789");
+        boolean res = m.matches();
+        System.out.println(res);
     }
-    public  static void main(String[] args) {
-       
-
-
-    }
-
 }
