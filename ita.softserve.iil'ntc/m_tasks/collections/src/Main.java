c048f624ddc92798de50598186e464f37ffaaca4
igor <ihor.ilnytskyi@gmail.com>
2015-05-02 18:31:28 +0300

    Folder is charged.

---
+++ ita.softserve.iil'ntc/m_tasks/collections/src/Main.java
@@ -0,0 +1,66 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Verbi gratia we have standard list of fruits & vegetables which is downloaded from Internet.
+        String standardFruitsAndVegetablesList = "Cucumber, cucumber, cucumber, cucumber, " +
+                "tomato, cucumber, grape, broccoli, " +
+                "eggplant, onion, watermelon, melon, cucumber, etc.";
+
+        //We are preparing our list for next manipulations.
+        standardFruitsAndVegetablesList = standardFruitsAndVegetablesList.toLowerCase();
+        standardFruitsAndVegetablesList = standardFruitsAndVegetablesList.replaceAll(",", "");
+        standardFruitsAndVegetablesList = standardFruitsAndVegetablesList.replace("etc.", "");
+
+        //We do an array of fruits and vegetables to use them for objects initialization.
+        String[] goodsStringList = standardFruitsAndVegetablesList.split("\\s+");
+
+        //We initialize LinkedList of goods.
+        LinkedList<FruitsAndVegetablesNomenclature> goodsLinkedList = new LinkedList<>();
+
+        //Passing objects to LinkedList.
+        for (int i = 0; i < goodsStringList.length; i++) {
+            goodsLinkedList.add(i, new FruitsAndVegetablesNomenclature(i, goodsStringList[i]));
+        }
+        System.out.println("At start list size is: " + goodsLinkedList.size() + ".");
+
+        //Doings with LinkedList.
+        //Different changes in LinkedList.
+        goodsLinkedList.add(new FruitsAndVegetablesNomenclature(155, "unknown fruit"));
+        goodsLinkedList.add(new FruitsAndVegetablesNomenclature(1016, "unknown vegetable"));
+        goodsLinkedList.add(new FruitsAndVegetablesNomenclature(1017, "unknown vegetable"));
+        goodsLinkedList.add(15, new FruitsAndVegetablesNomenclature(1111, "unknown fruit"));
+        System.out.println("After changes list size is: " + goodsLinkedList.size() + ".");
+
+        Iterator it = goodsLinkedList.iterator();
+
+        //Also we are going to change LinkedList with iterator.
+        int forIteration = 0;
+        while(it.hasNext()){
+            if (goodsLinkedList.get(forIteration).lookForObjectName().equals("cucumber")) {
+                it.next();
+                it.remove();
+            } else {
+                it.next();
+                forIteration++;
+            }
+        }
+
+        System.out.println("Now list size is: " + goodsLinkedList.size() + ".");
+
+        //We wanna change objects by adding shop name (Good Shop).
+        for (int i=0; i < goodsLinkedList.size(); i++) {
+            goodsLinkedList.get(i).changeObjectName("'Good Shop': " + goodsLinkedList.get(i).lookForObjectName() + ".");
+        }
+        System.out.println("Our list looks as following:");
+
+        for (int i=0; i < goodsLinkedList.size(); i++){
+            System.out.println(goodsLinkedList.get(i));
+        }
+
+    }
+
+}
+
---
+++ ita.softserve.iil'ntc/m_tasks/regexp/src/PhoneBookCheckingClass.java
@@ -0,0 +1,37 @@
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneBookCheckingClass {
+
+    public static void main (String [] args) {
+
+        TreeMap<Integer, String> myPhoneBook = new TreeMap<>();
+        myPhoneBook.put(1, "123?123?7777");
+        myPhoneBook.put(2, "(123) 456.7899");
+        myPhoneBook.put(3, "(123).456.7899");
+        myPhoneBook.put(4, "(123)-456-7899");
+        myPhoneBook.put(5, "123-456-7899");
+        myPhoneBook.put(6, "123 456 7899");
+        myPhoneBook.put(7, "123.456.7899");
+        myPhoneBook.put(8, "1234567899");
+        myPhoneBook.put(9, "fu*k");
+        myPhoneBook.put(10, "HNY");
+        myPhoneBook.put(11, "www.google.com.ua");
+        myPhoneBook.put(12, "(123)x456x7899");
+        myPhoneBook.put(13, "a12345678991");
+
+        for (Map.Entry<Integer, String> phoneNumber : myPhoneBook.entrySet()) {
+            System.out.println(phoneNumber.getKey() + " number - " + phoneNumber.getValue() + " - " + checkThePhone(phoneNumber.getValue()) + " number.");
+        }
+
+    }
+
+    public static boolean checkThePhone(String phoneNumberValue) {
+        Pattern regexTemplate = Pattern.compile("(^(\\(?\\d{3}\\)?)((\\.\\d{3}\\.)|(\\-\\d{3}\\-)|(\\s\\d{3}\\s))\\d{4})|\\d{10}");
+        Matcher inputtedPhoneNumberExample = regexTemplate.matcher(phoneNumberValue);
+        return inputtedPhoneNumberExample.matches();
+    }
+
+}
