a03a82b1227e5e8c02afaec26971c8508f0a75d4
ASI Home <asi-info@ukr.net>
2015-05-04 15:46:35 +0300

    New structure created

---
+++ ita.softserve.sazartc/OOP Task/src/ita/softserve/sazartc/Employee.java
@@ -0,0 +1,48 @@
+package ita.softserve.sazartc;
+
+/**
+ * Abstract class described Emplyee' properties with cached salary
+ */
+abstract public class Employee implements Comparable<Employee> {
+	
+	protected int id;
+	protected String name;
+	protected String surname;
+	protected double salaryBase;
+	
+	public Employee(int id, String surname, String name, double salaryBase) {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.salaryBase = salaryBase;
+	}
+	
+	abstract public double calculateSalary();
+
+	@Override
+	public String toString() {		
+		return String.format("[%d] %s %s: %.2f", 
+				this.id, this.surname, this.name, calculateSalary()); 
+	}
+
+	@Override
+	public int compareTo(Employee employee) {
+		if (this.calculateSalary() < employee.calculateSalary()) {
+			return -1;
+		} else if (this.calculateSalary() > employee.calculateSalary()) {
+			return 1;
+		} else {
+			if (this.surname.compareTo(employee.surname) < 0) {
+				return -1;
+			} else if (this.surname.compareTo(employee.surname) > 0) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	}
+	
+   
+	
+}
+
---
+++ ita.softserve.sazartc/OOP Task/src/ita/softserve/sazartc/Staff.java
@@ -0,0 +1,120 @@
+package ita.softserve.sazartc;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * A catalog of Employees' data
+ */
+public class Staff {
+	
+	private List<Employee> employees = new ArrayList<Employee>();
+	
+	public void addEmployee(Employee employee) {
+		employees.add(employee);
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder result = new StringBuilder();
+		result.append("==================================\n");
+		result.append("Employees' Salary List\n");
+		result.append("==================================\n");
+		for (Employee employee: this.employees) {
+			result.append(employee.toString()); 
+			result.append("\n");
+		}
+		result.append("==================================\n");
+		return result.toString();
+	}
+	
+	public String toString(int index) {
+		int fromIndex;
+		int toIndex;
+		String title;
+		
+		if (index > 0) {
+			fromIndex = 0;
+			toIndex = index;
+			title = String.format("First %d", index);
+		} else if (index < 0) {
+			fromIndex = this.employees.size() + index;
+			toIndex = this.employees.size();
+			title = String.format("Last %d", index * (-1));
+		} else {
+			throw new IllegalArgumentException();
+		}
+		
+		StringBuilder result = new StringBuilder();
+		result.append("==================================\n");
+		result.append(String.format("Employees' Salary List (%s)\n", title));
+		result.append("==================================\n");
+		for (Employee employee: this.employees.subList(fromIndex, toIndex)) {
+			result.append(employee.toString()); 
+			result.append("\n");
+		}
+		result.append("==================================\n");		
+		return result.toString();
+	}
+
+	public void sort() {
+		Collections.sort(this.employees);
+	}
+	
+	public void saveToFile(String fileName) throws IOException {
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter(fileName, false);
+			for (Employee employee: employees) {
+				writer.write(String.format("%s,,,", employee.id));
+				writer.write(String.format("%s,,,", employee.surname)); 
+				writer.write(String.format("%s,,,", employee.name)); 
+				writer.write(String.format("%f\n", employee.salaryBase));
+			}
+		} catch (IOException e) {
+            System.out.println(e.getMessage());
+	    } finally {
+	    	if (writer != null) writer.close();
+	    }
+		
+	}
+	
+	public void loadFromFile(String fileName, String initType) throws IOException {		
+		
+		List<Employee> tmpEmployees = new ArrayList<Employee>();
+		BufferedReader reader = null;		
+		try {
+			reader = new BufferedReader(new FileReader(fileName));
+			while (true) {
+				String line = reader.readLine();
+				if (line == null) break;
+
+				String[] lineArr = line.split(",,,");
+				
+				if (initType.equals("PER_HOUR")) {
+					tmpEmployees.add(new PerHourEmployee(Integer.parseInt(lineArr[0]), 
+							lineArr[1], lineArr[2], Double.parseDouble(lineArr[3])));
+				} else if (initType.equals("FIXED_RATE")) {
+					tmpEmployees.add(new FixedRateEmployee(Integer.parseInt(lineArr[0]), 
+							lineArr[1], lineArr[2], Double.parseDouble(lineArr[3])));
+				} else throw new IllegalArgumentException();	
+			}
+			
+			this.employees.clear();
+			this.employees = tmpEmployees;
+			
+		} catch (IOException e) {
+            System.out.println(e.getMessage());
+	    } finally {
+	    	if (reader != null) reader.close();
+	    }
+		
+	}
+
+
+}
