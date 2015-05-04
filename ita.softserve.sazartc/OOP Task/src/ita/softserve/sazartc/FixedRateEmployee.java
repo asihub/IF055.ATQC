7c534a74eb5b8a9966d6d85bb490af8cc8204400
ASI Home <asi-info@ukr.net>
2015-05-04 16:05:35 +0300

    Task 2. v0.93 - Changed tabs to spaces

--- ita.softserve.sazartc/OOP Task/src/ita/softserve/sazartc/FixedRateEmployee.java
+++ ita.softserve.sazartc/OOP Task/src/ita/softserve/sazartc/FixedRateEmployee.java
@@ -1,19 +1,18 @@
 package ita.softserve.sazartc;
 
 /**
- * Child Employee class described Employee with fixed salary calculated
- * on monthly wage
+ * Child Employee class described Employee with fixed salary calculated on
+ * monthly wage
  */
 public class FixedRateEmployee extends Employee {
-	
-	FixedRateEmployee(int id, String name, String surname, double salaryBase) {
-		super(id, name, surname, salaryBase);
-	}	
-	
-	@Override
-    public double calculateSalary() {
-		return this.salaryBase;
-	}
 
-	
+    FixedRateEmployee(int id, String name, String surname, double salaryBase) {
+        super(id, name, surname, salaryBase);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return this.salaryBase;
+    }
+
 }
--- ita.softserve.sazartc/OOP Task/src/ita/softserve/sazartc/Task2.java
+++ ita.softserve.sazartc/OOP Task/src/ita/softserve/sazartc/Task2.java
@@ -4,21 +4,21 @@ import java.io.IOException;
 
 public class Task2 {
 
-	public static void main(String[] args) throws IOException {		
-		
-		Staff perHourStaff = new Staff();
-		perHourStaff.loadFromFile("PerHourStaff.txt", "PER_HOUR");
-		System.out.println(perHourStaff.toString());
-		perHourStaff.sort();
-		System.out.println(perHourStaff.toString(5));
-		System.out.println(perHourStaff.toString(-3));		
+    public static void main(String[] args) throws IOException {
 
-		Staff fixedRateStaff = new Staff();
-		fixedRateStaff.loadFromFile("FixedRateStaff.txt", "FIXED_RATE");
-		System.out.println(fixedRateStaff.toString());
-		fixedRateStaff.sort();
-		System.out.println(fixedRateStaff.toString(5));
-		System.out.println(fixedRateStaff.toString(-3));        
-	}
-	
+        Staff perHourStaff = new Staff();
+        perHourStaff.loadFromFile("PerHourStaff.txt", "PER_HOUR");
+        System.out.println(perHourStaff.toString());
+        perHourStaff.sort();
+        System.out.println(perHourStaff.toString(5));
+        System.out.println(perHourStaff.toString(-3));
+
+        Staff fixedRateStaff = new Staff();
+        fixedRateStaff.loadFromFile("FixedRateStaff.txt", "FIXED_RATE");
+        System.out.println(fixedRateStaff.toString());
+        fixedRateStaff.sort();
+        System.out.println(fixedRateStaff.toString(5));
+        System.out.println(fixedRateStaff.toString(-3));
+    }
+
 }
