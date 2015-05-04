7e7ddf5d43bae7eab47c790ce4a1aac255c9e441
igor <ihor.ilnytskyi@gmail.com>
2015-05-04 13:20:07 +0300

    Another map was used and other changes.

--- ita.softserve.iil'ntc/m_tasks/collections/src/Main.java
+++ ita.softserve.iil'ntc/m_tasks/collections/src/Main.java
@@ -1,17 +1,19 @@
 import java.awt.*;
 import java.util.Iterator;
+import java.util.List;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
     public static void main(String[] args) {
 
-        //List block.
         //Verbi gratia we have standard list of fruits & vegetables which is downloaded from Internet.
-        String standardFruitsAndVegetablesList = "Cucumber, cucumber, cucumber, cucumber, " +
-                "tomato, cucumber, grape, broccoli, " +
-                "eggplant, onion, watermelon, melon, cucumber, etc.";
+        String standardFruitsAndVegetablesList = "Orange, mandarin, grapefruit, lemon, lime, kiwi, pineapple, " +
+                "pomegranate, avocado, pomelo, coconut, melon, banana, pear, peach, plum, quince, apricot, " +
+                "watermelon, cherry, blueberry, apple, strawberry, grape, fig, pitaya, papaya, raspberry, " +
+                "cauliflower, chive, fennel, garlic, ginger, onion, leek, mushroom, parsnip, potato, shallot, " +
+                "tomato, cucumber, broccoli, eggplant, etc.";
 
         //We are preparing our list for next manipulations.
         standardFruitsAndVegetablesList = standardFruitsAndVegetablesList.toLowerCase();
@@ -21,32 +23,33 @@ public class Main {
         //We do an array of fruits and vegetables to use them for objects initialization.
         String[] goodsStringList = standardFruitsAndVegetablesList.split("\\s+");
 
+        //List block.
         //We initialize LinkedList of goods.
-        LinkedList<FruitsAndVegetablesNomenclature> goodsLinkedList = new LinkedList<>();
+        List <FruitsAndVegetablesNomenclature> goodsList = new LinkedList<>();
 
-        //Passing objects to LinkedList.
+        //Passing objects into list.
         for (int i = 0; i < goodsStringList.length; i++) {
-            goodsLinkedList.add(i, new FruitsAndVegetablesNomenclature(i, goodsStringList[i]));
+            goodsList.add(i, new FruitsAndVegetablesNomenclature(i, goodsStringList[i]));
         }
-        System.out.println("At start list size is: " + goodsLinkedList.size() + ".");
 
         //We are printing our list with "for-each" loop.
         System.out.println("Our list looks like following:");
-        for (FruitsAndVegetablesNomenclature goods : goodsLinkedList) System.out.println(goods);
 
-        //Doings with LinkedList.
-        //Different changes in LinkedList.
-        goodsLinkedList.add(new FruitsAndVegetablesNomenclature(155, "unknown fruit"));
-        goodsLinkedList.add(new FruitsAndVegetablesNomenclature(1016, "unknown vegetable"));
-        goodsLinkedList.add(new FruitsAndVegetablesNomenclature(1017, "unknown vegetable"));
-        goodsLinkedList.add(15, new FruitsAndVegetablesNomenclature(1111, "unknown fruit"));
-        System.out.println("After new objects adding list size is: " + goodsLinkedList.size() + ".");
+        for (FruitsAndVegetablesNomenclature goods : goodsList) System.out.println(goods);
 
-        //Also we are going to change LinkedList with iterator.
-        Iterator it = goodsLinkedList.iterator();
+        //Doings with list.
+        //Different changes in list.
+        System.out.println("Now we are going to change our list: \n1)add few objects; \n" +
+                "2)remove cucumber; \n3)add shop's name - 'Good Shop'.");
+
+        goodsList.add(new FruitsAndVegetablesNomenclature(1016, "unknown vegetable"));
+        goodsList.add(15, new FruitsAndVegetablesNomenclature(1111, "unknown fruit"));
+
+        //Also we are going to change list with iterator.
+        Iterator it = goodsList.iterator();
         int forIteration = 0;
         while(it.hasNext()){
-            if (goodsLinkedList.get(forIteration).lookForObjectName().equals("cucumber")) {
+            if (goodsList.get(forIteration).lookForObjectName().equals("cucumber")) {
                 it.next();
                 it.remove();
             } else {
@@ -55,20 +58,19 @@ public class Main {
             }
         }
 
-        System.out.println("After changes with iterator list size is: " + goodsLinkedList.size() + ".");
-
-        //We wanna change objects by adding shop name (Good Shop).
-        for (int i=0; i < goodsLinkedList.size(); i++) {
-            goodsLinkedList.get(i).changeObjectName("'Good Shop': " + goodsLinkedList.get(i).lookForObjectName() + ".");
+        //We wanna change objects by adding shop name - 'Good Shop'.
+        for (int i=0; i < goodsList.size(); i++) {
+            goodsList.get(i).changeObjectName("'Good Shop': " + goodsList.get(i).lookForObjectName() + ".");
         }
 
         //We are printing our list with "for-each" loop.
-        System.out.println("After adding shop's name list looks as following:");
-        for (FruitsAndVegetablesNomenclature goods : goodsLinkedList) System.out.println(goods);
+        System.out.println("After all changes our list looks like following:");
+
+        for (FruitsAndVegetablesNomenclature goods : goodsList) System.out.println(goods);
 
         //Map block.
         //Map initialization.
-        TreeMap <Integer, FruitsAndVegetablesNomenclature> goodsMap = new TreeMap<>();
+        Map <Integer, FruitsAndVegetablesNomenclature> goodsMap = new ConcurrentHashMap<>();
 
         //We are adding our objects to map.
         for(int i=0; i < goodsStringList.length; i++){
@@ -76,37 +78,38 @@ public class Main {
         }
 
         //Printing map.
-        System.out.println("Our map has " + goodsMap.size() + " elements and looks like following:");
+        System.out.println("Our map looks like following:");
+
         for(Map.Entry<Integer, FruitsAndVegetablesNomenclature> map : goodsMap.entrySet()) {
-            System.out.println(map.getKey() + ":---> " + map.getValue());
+            System.out.println(map.getKey() + " in map---> " + map.getValue());
         }
 
+        //Changing map.
         //Deleting object with key "2" in map.
+        System.out.println("Now we are going to change our map: \n1)delete the second object; \n" +
+                "2)remove melon; \n3)change name from onion to bow.");
+
         Integer elementKeyForDeleting = 2;
-        System.out.println("We are deleting the " + elementKeyForDeleting + " element from map.");
+
         for (Iterator <Integer> itt = goodsMap.keySet().iterator(); itt.hasNext();) {
             if(itt.next().equals(elementKeyForDeleting)) itt.remove();
         }
 
-        //Map after deleting the second element.
-        System.out.println("Our map:");
+        //Deleting melon.
         for(Map.Entry<Integer, FruitsAndVegetablesNomenclature> map : goodsMap.entrySet()) {
-            System.out.println(map.getKey() + ":---> " + map.getValue());
+            if (map.getValue().lookForObjectName().equals("melon")) goodsMap.remove(map.getKey());
         }
 
-        System.out.println("We are deleting cucumbers from map.");
-        Iterator <Map.Entry<Integer, FruitsAndVegetablesNomenclature>> iter = goodsMap.entrySet().iterator();
-        while (iter.hasNext()) {
-            Map.Entry<Integer, FruitsAndVegetablesNomenclature> entry = iter.next();
-            if("cucumber".equals(entry.getValue().lookForObjectName())){
-                iter.remove();
-            }
+        //Changing name.
+        for(Map.Entry<Integer, FruitsAndVegetablesNomenclature> map : goodsMap.entrySet()) {
+            if (map.getValue().lookForObjectName().equals("onion")) goodsMap.get(map.getKey()).changeObjectName("bow");
         }
 
-        //Map after other deleting.
-        System.out.println("Our map:");
+        //Map after our changes.
+        System.out.println("Our map after changes:");
+
         for(Map.Entry<Integer, FruitsAndVegetablesNomenclature> map : goodsMap.entrySet()) {
-            System.out.println(map.getKey() + ":---> " + map.getValue());
+            System.out.println(map.getKey() + " in map---> " + map.getValue());
         }
 
     }
