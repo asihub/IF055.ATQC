e7c68618d6dc43bc9cefa07cbffa13e48852f350
igor <ihor.ilnytskyi@gmail.com>
2015-05-21 13:54:45 +0300

    try

---
+++ WebDriverTask/src/main/java/iilntc/TestGoogle.java
@@ -0,0 +1,16 @@
+package iilntc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Ihor Ilnytskyi on 21.05.2015.
+ */
+public class TestGoogle {
+
+
+    @Test
+    public void testGoogleSite(){
+        Assert.assertTrue(2 == 2);
+    }
+}
