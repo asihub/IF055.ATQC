fbda6761e4ec94ed398026802f3a5e181207c6ff
ihor-dynka <ihordynka@gmail.com>
2015-05-27 02:05:31 +0300

    TODOs fixed

--- src/main/java/idyntc/task_oop/Task2.java
+++ src/main/java/idyntc/task_oop/Task2.java
@@ -18,7 +18,7 @@ import java.util.List;
  */
 public class Task2 {
     public static void main(String[] args) {
-        String filePath = "src\\main\\resources\\";
+        String filePath = "src\main\java\idyntc\task_oop\";
         String perHourSalaryFile = filePath + "inputData\\perHourSalaryInput.csv";
         String fixedSalaryFile = filePath + "inputData\\fixedSalaryInput.csv";
 
@@ -27,33 +27,24 @@ public class Task2 {
 
         CSVFileReader csvFileReader = new CSVFileReader();
         CSVFileWriter csvFileWriter = new CSVFileWriter();
-        //TODO a lot of code & comments, refactor
-// reading from file
-        csvFileReader.readFromFile(perHourSalaryEmployeeList, perHourSalaryFile, "perHourSalary");
-// sorting this list
-        perHourSalaryEmployeeList.sort(new EmployeesComparator());
-// writing in console and file sorted perHourSalaryList
-        Employee.printEmployeeFromRange(perHourSalaryEmployeeList, 0, 9);
-        csvFileWriter.writeFile(perHourSalaryEmployeeList.subList(0, 9), filePath + "outputData/perHourSalaryOutput.csv");
-// writing in console and file from 0th to 4th names from sorted perHourSalaryList
-        Employee.printEmployeeFromRange(perHourSalaryEmployeeList, 0, 4, "name");
-        csvFileWriter.writeFile(perHourSalaryEmployeeList.subList(0, 5), filePath + "outputData/5NameOfPerHourSalaryOutput.csv");
-// writing in console and file from 7th to 9th elements from sorted perHourSalaryList
-        Employee.printEmployeeFromRange(perHourSalaryEmployeeList, 7, 9, "id");
-        csvFileWriter.writeFile(perHourSalaryEmployeeList.subList(7, 10), filePath + "outputData/Last3IDofPerHourSalaryOutput.csv");
+        
 
-// reading from file
+        csvFileReader.readFromFile(perHourSalaryEmployeeList, perHourSalaryFile, "perHourSalary");
+
+        perHourSalaryEmployeeList.sort(new EmployeesComparator());
+       
+        csvFileWriter.writeFile(perHourSalaryEmployeeList.subList(0, 9), filePath + "outputData/perHourSalaryOutput.csv");
+        csvFileWriter.writeFile(perHourSalaryEmployeeList.subList(0, 5), filePath + "outputData/5NameOfPerHourSalaryOutput.csv");
+        csvFileWriter.writeFile(perHourSalaryEmployeeList.subList(7, 10), filePath + "outputData/Last3IDofPerHourSalaryOutput.csv");
+        
         csvFileReader.readFromFile(fixedSalaryEmployeeList, fixedSalaryFile, "fixedSalary");
-// sorting this list
+
         fixedSalaryEmployeeList.sort(new EmployeesComparator());
-// writing in console and file sorted fixedSalaryList
-        Employee.printEmployeeFromRange(fixedSalaryEmployeeList, 0, 9);
+
         csvFileWriter.writeFile(fixedSalaryEmployeeList.subList(0, 9), filePath + "outputData/fixedSalaryOutput.csv");
-// writing in console and file from 0th to 4th names from sorted fixedSalaryList
-        Employee.printEmployeeFromRange(fixedSalaryEmployeeList, 0, 4, "name");
+
         csvFileWriter.writeFile(fixedSalaryEmployeeList.subList(0, 5), filePath + "outputData/5NameOfFixedSalaryOutput.csv");
-// writing in console and file from 7th to 9th elements from sorted fixedSalaryList
-        Employee.printEmployeeFromRange(fixedSalaryEmployeeList, 7, 9, "id");
+
         csvFileWriter.writeFile(fixedSalaryEmployeeList.subList(7, 10), filePath + "outputData/Last3IDofFixedSalaryOutput.csv");
 
 
