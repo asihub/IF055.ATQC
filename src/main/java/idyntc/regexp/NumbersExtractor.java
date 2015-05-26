73f5f7d20099677c22e1c3baa3a845357cf04c77
Ihor Dynka <ihordynka@gmail.com>
2015-05-27 00:15:14 +0300

    TODOs fixed

---
+++ src/main/java/idyntc/regexp/NumbersExtractor.java
@@ -0,0 +1,31 @@
+package idyntc.regexp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//TODO rename
+public class NumbersExtractor {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("\\d*\\.?\\d+");
+        Matcher matcher = pattern.matcher("Hello 2.15 digital World 5,3");
+
+        List<String> numbers=new ArrayList<String>();
+
+        while (matcher.find()){
+            numbers.add(matcher.group());
+        }
+        if (numbers.isEmpty()){
+            System.out.println("There are no numbers in this string");
+        }else {
+            System.out.println("Numbers from this string: " +numbers);
+        }
+
+    }
+}
+
+
+
+
+
