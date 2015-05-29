86571b1d9e6cb07addedcd3db901f34b0a8d57be
ASI <asi-info@ukr.net>
2015-05-29 16:15:50 +0300

    google_test v0.93 - GoogleTestRunner todos fixed

---
+++ src/main/java/sazartc/google_test/tests/GoogleTestsRunner.java
@@ -0,0 +1,35 @@
+package sazartc.google_test.tests;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Runner for tests. Initializes and releases resources
+ */
+public abstract class GoogleTestsRunner {
+    protected WebDriver driver;
+    protected static final Logger log = Logger.getLogger(GoogleTester.class);
+
+    @BeforeTest
+    public void setUp() {
+
+        log.info("--- Tests started --->");
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void tearDown() throws Exception {
+        Thread.sleep(3000);
+        //driver.quit();
+        log.info("<--- Tests finished ---");
+    }
+}
+
+
