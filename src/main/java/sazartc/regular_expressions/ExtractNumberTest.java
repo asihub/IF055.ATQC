6b3ebf4737469c1f303e14741ee994318c671524
ASI Home <asi-info@ukr.net>
2015-05-30 17:11:32 +0300

    regular expressions reviewed

---
+++ src/main/java/sazartc/regular_expressions/ExtractNumberTest.java
@@ -0,0 +1,31 @@
+package sazartc.regular_expressions;
+
+import sazartc.common_libraries.CsvReader;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractNumberTest {
+    public static void main(String[] args) {
+        final String SENTENCIES_WITH_NUMBERS_FILENAME =
+                "src\\main\\java\\sazartc\\regexp\\resources\\sentenciesWithNumbers.csv";
+        final String NUMBER_PATTERN = "\\d+\\.?\\d*";
+        Pattern pattern = Pattern.compile(NUMBER_PATTERN);
+
+        List<String> sentenciesWithNumbers = CsvReader.INSTANCE.readFromCsv(SENTENCIES_WITH_NUMBERS_FILENAME);
+
+        for (String sentencyWithNumbers: sentenciesWithNumbers) {
+            Matcher matcher = pattern.matcher(sentencyWithNumbers);
+            System.out.printf("Extracted numbers from [%40s]: ", sentencyWithNumbers);
+            while (matcher.find()) {
+                System.out.printf("%s ", matcher.group());
+            }
+            System.out.println();
+        }
+
+    }
+
+}
+
+
