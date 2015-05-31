10f2275615610c15da0ccf771d3ca93c7dc2c97c
Ihor Dynka <ihordynka@gmail.com>
2015-05-31 11:00:28 +0300

    TODOs fixed

--- src/main/java/idyntc/regexp/NumberExtractor.java
+++ src/main/java/idyntc/regexp/NumberExtractor.java
@@ -6,26 +6,32 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NumberExtractor {
-    public static void main(String[] args) {
-        //TODO what's that?
-        String reg_exp_pattern = "\\d*\\.?\\d+";
+    private static final String NUMBER_PATTERN = "\\d*\\.?\\d+";
 
-        Pattern pattern = Pattern.compile(reg_exp_pattern);
-        Matcher matcher = pattern.matcher("Hello 2.15 digital World 5,3");
+    public void numberExtract(String text) {
+        List<String> numbersList = new ArrayList<>();
 
-        List<String> numbers = new ArrayList<String>();
+        Pattern pattern = Pattern.compile(NUMBER_PATTERN);
+        Matcher matcher = pattern.matcher(text);
 
         while (matcher.find()) {
-            numbers.add(matcher.group());
+            numbersList.add(matcher.group());
         }
-        if (numbers.isEmpty()) {
+        if (numbersList.isEmpty()) {
             System.out.println("There are no numbers in this string");
         } else {
-            System.out.println("Numbers from this string: " + numbers);
+            System.out.println("Numbers from this string: " + numbersList);
         }
+    }
 
+    public static void main(String[] args) {
+        NumberExtractor numberExtractor = new NumberExtractor();
+        numberExtractor.numberExtract("Hello 2.15 digital World 5,3");
     }
 }
+
+
+
 
 
 
