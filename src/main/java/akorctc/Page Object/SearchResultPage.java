41726476557a3425449ec3cd1ebadab8069968bb
Antracot <Artem.korchevy@gmail.com>
2015-06-01 16:59:38 +0300

    Add Page Object

---
+++ src/main/java/akorctc/Page Object/SearchResultPage.java
@@ -0,0 +1,17 @@
+package akorctc;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SearchResultPage extends Base  {
+    public SearchResultPage(WebDriver driver) {
+        super(driver);
+    }
+    public WebElement getFirstLink(){
+       return driver.findElement(By.cssSelector(".r>a"));
+    }
+    public ImagesPage clickImageLink(){
+        driver.findElement(By.xpath(".//*[@id='hdtb-msb']/div[2]/a")).click();
+        return new ImagesPage(driver);
+    }}
