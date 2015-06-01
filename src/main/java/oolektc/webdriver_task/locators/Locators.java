298020ca67d5c9bfd5374ad5aa4d0baa482b1f6f
ostap-oleksyn <ostap.oleksyn@gmail.com>
2015-06-01 16:07:26 +0300

    Fixed images page logo locator

--- src/main/java/oolektc/webdriver_task/locators/Locators.java
+++ src/main/java/oolektc/webdriver_task/locators/Locators.java
@@ -11,6 +11,6 @@ public final class Locators {
     public static final By IMAGES_TAB = By.xpath("(.//*[@id='hdtb-msb']//a)[1]");
     public static final By IMAGES_PAGE_IMAGE = By.xpath("(.//*[@id='rg_s']//img)");
 
-    public static final By HEADER_GOOGLE_LOGO = By.xpath(".//*[@id='logo']");
+    public static final By HEADER_GOOGLE_LOGO = By.xpath("(.//*[@id='searchform']//img)[1]");
 
 }
